Fix numerical scheme for zeta

src/simcardems2/land_Zetasplit.py
@@ -18,21 +18,19 @@ class Scheme(str, Enum):
 
 def _Zeta(Zeta, A, c, dLambda, dt, scheme: Scheme):
     if scheme == Scheme.analytic:
-        if dt == 0:
-            print("NB!: dt equal to zero in forward step! This is expected for initialisation step")
-            print("dLambda:", dLambda)
-            return Zeta
+        dZetas_dt = A * dLambda - Zeta * c
+        dZetas_dt_linearized = -c
+        if abs(c) > 1e-8:
+            return Zeta + dZetas_dt * (np.exp(-c * dt) - 1.0) / dZetas_dt_linearized
         else:
-            return (np.where(
-                    (np.abs(-c) > 1e-08),
-                    Zeta * np.exp(-c * dt) + (A * dLambda / (c * dt)) * (1.0 - np.exp(-c * dt)), 
-                    Zeta + A*dLambda - Zeta*c*dt
-                    )
-                )
+            # Forward euler
+            return Zeta + dZetas_dt * dt
+
     elif scheme == Scheme.bd:
         return Zeta + A * dLambda / (1.0 + c * dt)
     else:
         return Zeta * (1.0 - c * dt) + A * dLambda
+
 
 _parameters = {
     "Beta0": 2.3,
@@ -159,11 +157,9 @@ class LandModel(pulse.ActiveModel):
     def update_Zetas(self):
         logger.debug("update Zetas")
         self._Zetas.vector()[:] = _Zeta(
-            #self.Zetas_prev.vector(),
             self.Zetas_prev.vector().get_local(),
             self.As,
             self.cs,
-            #self.dLambda.vector(),
             self.dLambda.vector().get_local(),
             self.dt,
             self._scheme,
@@ -176,11 +172,9 @@ class LandModel(pulse.ActiveModel):
     def update_Zetaw(self):
         logger.debug("update Zetaw")
         self._Zetaw.vector()[:] = _Zeta(
-            #self.Zetaw_prev.vector(),
             self.Zetaw_prev.vector().get_local(),
             self.Aw,
             self.cw,
-            #self.dLambda.vector(),
             self.dLambda.vector().get_local(),
             self.dt,
             self._scheme,
@@ -235,8 +229,6 @@ class LandModel(pulse.ActiveModel):
             * (Tref * scale_popu_Tref / (rs * scale_popu_rs))
             * (self.XS * (self.Zetas + 1.0) + self.XW * self.Zetaw)
         )
-
-
 
     def Wactive(self, F, **kwargs):
         """Active stress energy"""